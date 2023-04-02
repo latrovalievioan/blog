5c9bb2145620a515934311ce683a36257d0444ab
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-04-02 23:08:45 +0300

    bump nav fontsize

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,7 +5,7 @@ export default function Nav() {
       <h1 className="text-9xl font-serif font-extrabold text-gray-800">
         IOAN{"'"}S BLOG
       </h1>
-      <div className="flex items-center justify-evenly w-full border-y-2 border-black">
+      <div className="text-xl flex items-center justify-evenly w-full border-y-2 border-black">
         <div className="date flex items-center justify-center gap-3 px-6">
           {date}
         </div>
